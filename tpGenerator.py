6dfeca2d66ec14b29957f38a2f4b561acdf3d06d
Henry Benjamin Akins <hakins@lxplus986.cern.ch>
2024-05-22 14:17:30 +0200

    waveform

--- tpGenerator.py
+++ tpGenerator.py
@@ -5,40 +5,19 @@ from include.TPFinder import TPFinder as TPFinder
 import sys
 
 #load waveform data (100 low energy electron events, 5 - 100 MeV)
-col = np.loadtxt("data/PedSubWaveform_Collection_Dump.txt")
+col = np.loadtxt("/eos/home-e/evilla/dune/sn-data/standard/aggregated_prodmarley_nue_spectrum_radiological_decay0_dune10kt_refactored_1x2x6_CC-WFdump-1events_thr30/waveforms_tryWfDumper.txt")
 ind = np.loadtxt("data/PedSubWaveform_Induction_Dump.txt")
+#
+col = col - 500
 
 event = 2
 threshold = 30
 
 #plot collection waveforms from event
-plt.figure(figsize = (10,6))
 offset = 0
 for data in col:
-    if (data[0]==event):
-        ADCS = data[2:]
-        plt.title("Collection waveforms (event %.0f)" %event)
-        plt.plot(ADCS + offset)#offset by channel number
+    ADCS = data[2:]
+    hits = TPFinder(ADCS, threshold)
+    print(hits)
+    #add hits to new row od textfile
        
-        hits = TPFinder(ADCS, threshold)
-        for hit in hits:
-            plt.plot(hit.time_start,hit.adc_peak + offset,'o')
-           
-        plt.text(len(ADCS) + 100, offset, 'channel : %.0f' %data[1])
-        offset+=100
-        plt.ylabel("Offset Amplitude [ADC]")
-        plt.xlabel("Time [ticks]")
-plt.show()
-
-#plot induction waveforms for event
-plt.figure(figsize = (10,6))
-for data in ind:
-    if (data[0]==event):
-        ADCS = data[2:]
-        plt.title("Induction waveforms (event %.0f)" %event)
-        plt.plot(ADCS + offset)#offset by channel number
-        plt.text(len(ADCS) + 100, offset, 'channel : %.0f' %data[1])
-        offset+=100
-        plt.ylabel("Offset Amplitude [ADC]")
-        plt.xlabel("Time [ticks]")
-plt.show()
