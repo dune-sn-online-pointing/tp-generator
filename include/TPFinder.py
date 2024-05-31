32f014951a4ceb227a3716af7153e3339df83896
Henry Benjamin Akins <hakins@lxplus980.cern.ch>
2024-05-31 15:50:26 +0200

    true

--- include/TPFinder.py
+++ include/TPFinder.py
@@ -4,7 +4,9 @@ from .TriggerPrimitive import TriggerPrimitive
 #-------------------------------------------
 #Primitive hit finding
 #-------------------------------------------
+#This is same function as in TPFinder, put here for testing
 def TPFinder(waveform, thresh):
+
     is_hit = False
 
     hit_charge =[]    
@@ -19,7 +21,18 @@ def TPFinder(waveform, thresh):
             temp_tick = tick
             while (waveform[temp_tick]>0):
                 temp_tick = temp_tick - 1
+            first_zero = temp_tick
+            mean_noise = np.mean(waveform[first_zero-10:first_zero])
+            temp_tick = tick
+            while (waveform[temp_tick]>mean_noise):
+                temp_tick = temp_tick - 1
             true_start = temp_tick
+                
+                
+            '''print(f"True Start: {true_start}")
+            print(f"True Start ADC: {waveform[true_start]}")
+            print(f"Time Start: {tick}")
+            print(f"Time Start ADC: {waveform[tick]}")'''
            
         if(is_hit == True):
             hit_charge.append(adc)
@@ -28,10 +41,14 @@ def TPFinder(waveform, thresh):
             time_end  = tick
             is_hit = False
             temp_tick_f = tick
-            while(waveform[temp_tick]>0):
+            while(waveform[temp_tick]>mean_noise):
                 temp_tick_f+=1
             true_end = temp_tick_f
-            
+            '''print(f"True End: {true_end}")
+            print(f"True End ADC: {waveform[true_end]}")
+            print(f"Time End: {tick}")
+            print(f"Time End ADC: {waveform[tick]}")
+            '''
             true_adc = 0
             for i in range(true_start,true_end):
                 true_adc = true_adc + waveform[i]
@@ -50,3 +67,4 @@ def TPFinder(waveform, thresh):
             this_hit = TriggerPrimitive()
             
     return hits,true_adc_areas #return set of hits for waveform
+    
